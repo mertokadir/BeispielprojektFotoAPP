6f6ace00dcaa9582dc7f7f4576a413b240dd31b0
Kadler23 <37613739+Kadler23@users.noreply.github.com>
2022-06-19 22:27:46 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class FotoAPPMain {
 
 static void main(String[] args){
 
-	System.out.println("Dies ist ein Beispiel für den Vortrag");
+	System.out.println("Dies ist ein Beispiel für den Vortrag!");
 	
   }
 }

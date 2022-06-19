790af6d7a4e0c7d1752153e5f8edf1e8a8340eea
mertokadir <37595976+mertokadir@users.noreply.github.com>
2022-06-19 22:25:16 +0200

    Beispiel hinzugefügt

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
-public class Test {
+public class FotoAPPMain {
 
 static void main(String[] args){
 
-	System.out.println("Hello World");
+	System.out.println("Dies ist ein Beispiel für den Vortrag");
 	
   }
 }
